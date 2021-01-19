ae14fd4489a2d4aacfeb3bd242d4e7e2b11098a4
root <shisuan@myweimai.com>
2021-01-19 23:25:58 +0800

    演示cookie成功

--- controllers/testlogin.go
+++ controllers/testlogin.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -12,17 +13,31 @@ func (c *TestLoginController) Login() {
 	name := c.Ctx.GetCookie("name")
 	password := c.Ctx.GetCookie("password")
 	if name != "" {
-
 		c.Ctx.WriteString("Username:" + name + ", password:" + password)
-	}
-	c.Ctx.WriteString(`<html><form action="http://127.0.0.1:8080/test_input">
+	} else {
+		c.Ctx.WriteString(`<html><form action="http://127.0.0.1:8080/test_login" method="POST">
 									<input type="text" name="Username" />
-									<input type="password" name="Password" />
+									<input type="password" name="pwd" />
 									<input type="Submit" value="提交" />
 									</form>
 								</html>`)
+	}
 }
+
+type UserInfo struct {
+	Username string
+	Password string `form:"pwd"`
+}
+
 func (c *TestLoginController) Post() {
+	u := UserInfo{}
+	if err := c.ParseForm(&u); err != nil {
+		//全局捕获异常然后退出
+		fmt.Println(err)
+	}
+	c.Ctx.SetCookie("name", u.Username, 100, "/")
+	c.Ctx.SetCookie("password", u.Password, 100, "/")
+	c.Ctx.WriteString("username:" + u.Username + ",password:" + u.Password)
 
 }
 func (c *TestLoginController) Get() {
--- routers/router.go
+++ routers/router.go
@@ -8,6 +8,6 @@ import (
 func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/test_input", &controllers.TestInputController{}, "get:Get;post:Post")
-	beego.Router("/test_login", &controllers.TestLoginController{}, "get:Login")
+	beego.Router("/test_login", &controllers.TestLoginController{}, "get:Login;post:Post")
 
 }
